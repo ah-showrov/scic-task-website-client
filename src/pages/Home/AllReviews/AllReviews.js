ae0d1ac5b5808b17a979f525c74299172ce907f4
Showrov <ahshowrov1@gmail.com>
2021-12-18 09:37:08 +0600

    git final commit

--- src/pages/Home/AllReviews/AllReviews.js
+++ src/pages/Home/AllReviews/AllReviews.js
@@ -17,7 +17,7 @@ const AllReviews = () => {
     <div className="allReviews-container">
       <Container className="w-100 reviews-wrapper ">
         <h1 className=" text-uppercase text-center fw-bold ps-3 mb-5">
-          <span className="text-danger">Customers</span> Review{" "}
+          <span className="text-danger">Customers</span> Reviews{" "}
         </h1>
 
         <Row className="g-5 w-100 mx-auto mt-5">
